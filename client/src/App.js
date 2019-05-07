abfafc8378417546725beb69725a5873175fd0b7
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-07 14:41:12 -0700

    saveMovie functional

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,11 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
 
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" render={props => <Movie {...props} />} />
+        <Route 
+          path="/movies/:id" 
+          render={props => <Movie {...props} addToSavedList={this.addToSavedList} />} 
+          
+          />
       </div>
     );
   }
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -63,7 +63,7 @@ export default class Movie extends Component {
             </div>
           ))}
         </div>
-        <div className="save-button">Save</div>
+        <div className="save-button" onClick={this.saveMovie}>Save</div>
       </div>
     );
   }
